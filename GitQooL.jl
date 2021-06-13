18ed28d13fc4acaf902a3a28a79bac2dac2934cb
Gregory Kemp <gregory.kemp@core-uk.com>
2021-06-13 21:31:14 +0100

    Added main function and parse_commandline() to parameterise login/db details using command line arguments

--- GitQooL.jl
+++ GitQooL.jl
@@ -1,37 +1,83 @@
+using ArgParse
 using ODBC
 using DataFrames
 using LightXML
 
-println("Enter user ID for IVIEWALPHA:")
-userid = readline()
-println("Enter password:")
-password = readline()
+function parse_commandline()
+    s = ArgParseSettings()
 
-ODBC.adddsn(
-    "gk-db01_IVIEWALPHA",
-    "SQL Server";
-    SERVER = "gk-db01",
-    DATABASE = "IVIEWALPHA",
-    UID = userid,
-    PWD = password,
-)
-conn = ODBC.Connection("gk-db01_IVIEWALPHA", userid, password)
+    @add_arg_table! s begin
+        "--servername", "-s"
+        help = "server name on which your target database is stored"
+        arg_type = String
+        default = "gk-db01"
+        "--database", "-d"
+        help = "database name"
+        arg_type = String
+        default = "IVIEWALPHA"
+        "--username", "-u"
+        help = "sql server log in username"
+        arg_type = String
+        required = false
+        "--password", "-p"
+        help = "sql server log in password"
+        arg_type = String
+        required = false
+    end
 
-# Get all object names, types, and definitions from DB
-objectsql = read("sqlObjectDefinitions.sql",String)
-sqlobjects = DBInterface.execute(conn, objectsql) |> DataFrame
-
-# Loop through each object and store each definition in a sql file grouped in folders by type
-for (name, schemaname, type_desc, definition) in zip(
-    sqlobjects.name,
-    sqlobjects.schemaname,
-    sqlobjects.type_desc,
-    sqlobjects.definition,
-)
-    mkpath(type_desc)
-    filename = "$type_desc/$schemaname.$name.sql"
-    output_file = open(filename, "w")
-    #println(filename)
-    write(output_file, definition)
-    close(output_file)
+    return parse_args(s)
 end
+
+function main()
+    parsed_args = parse_commandline()
+
+    servername = parsed_args["servername"]
+    database = parsed_args["database"]
+    username = parsed_args["username"]
+    password = parsed_args["password"]
+
+    if username == nothing
+        println("Enter user ID for IVIEWALPHA:")
+        username = chomp(readline())
+    end
+
+    if password == nothing
+        println("Enter password:")
+        password = chomp(readline())
+    end
+
+    dnsname = "$(servername)_$(database)"
+
+    ODBC.adddsn(
+        dnsname,
+        "SQL Server";
+        SERVER = servername,
+        DATABASE = database,
+        UID = username,
+        PWD = password,
+    )
+    conn = ODBC.Connection(dnsname, username, password)
+
+    # Get all object names, types, and definitions from DB
+    objectsql = read("sqlObjectDefinitions.sql", String)
+    sqlobjects = DBInterface.execute(conn, objectsql) |> DataFrame
+
+    # Loop through each object and store each definition in a sql file grouped in folders by type
+    for (name, schemaname, type_desc, definition) in zip(
+        sqlobjects.name,
+        sqlobjects.schemaname,
+        sqlobjects.type_desc,
+        sqlobjects.definition,
+    )
+        mkpath(type_desc)
+        filename = "$type_desc/$schemaname.$name.sql"
+        output_file = open(filename, "w")
+        #println(filename)
+        write(output_file, definition)
+        close(output_file)
+    end
+
+    DBInterface.close!(conn)
+end
+
+main()
